4f215a0195533eb4ebfe76948e96cb01e35db622
Nam-H-Nguyen <nguyenbydoing@gmail.com>
2018-04-16 21:10:08 -0700

    setup c++ ide

---
+++ Week 2/Week 2 CPP/Week 2 CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Week 2 CPP
+//
+//  Created by NomNomNam on 4/16/18.
+//  Copyright © 2018 NamHNguyen NYU Tandon School of Engineering Bridge 2018 Program. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
