a42b1077b5f5b1e6d1cddec16777ed8c0264da7f
Nam-H-Nguyen <nguyenbydoing@gmail.com>
2018-06-01 19:04:56 -0700

    Finish dynamiicaly adding num to new array size through vector and memory allocation technique implemented in C++

--- Week 7 - Pointers and Dynamic Storage Bernoulli Random Variables/homework/nn1338_hw7_q6/nn1338_hw7_q6/nn1338_hw7_q6.cpp
+++ Week 7 - Pointers and Dynamic Storage Bernoulli Random Variables/homework/nn1338_hw7_q6/nn1338_hw7_q6/nn1338_hw7_q6.cpp
@@ -38,9 +38,105 @@ Please enter a number you want to search.
  */
 
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int main() {
+string* main1(int& outLinesArrSize);
+vector<string> main2();
+
+int main () {
+    string* linesArr;
+    int linesArrSize;
+    vector<string> linesVector;
+    
+    linesArr = main1(linesArrSize);
+    linesVector = main2();
+    
+    string searchNum;
+    
+    cout<<"Please enter a number you want to search.\n";
+    cin>>searchNum;
+    
+    cout<<"Version A:\n"
+        <<searchNum<<" shows in lines ";
+    
+    for (int i = 0; i < linesArrSize; i++) {
+        if (linesArr[i] == searchNum)
+            cout<<i + 1<<" ";
+    }
+    
+    cout<<endl;
+    
+    cout<<"Version B:\n"
+        <<searchNum<<" shows in lines ";
+    
+    for (int i = 0; i < linesVector.size(); i++) {
+        if (linesVector[i] == searchNum)
+            cout<<i + 1<<" ";
+    }
+    
+    cout<<endl;
     
     return 0;
+}
+
+string* main1(int& outLinesArrSize) {
+    string* lines;
+    int linesSize = 0;
+    bool seenEndOfSequence = false;
+    string currLine;
+    
+    lines = new string[linesSize];
+    
+    cout<<"Version A:\n";
+    cout<<"Please enter a sequence of positive integers, each in a separate line.\n"
+        <<"End you input by typing -1.\n";
+    
+    while (seenEndOfSequence == false) {
+        getline(cin, currLine);
+        if (currLine == "-1") // If the user types in -1, break the while loop
+            seenEndOfSequence = true;
+        else {
+            lines[linesSize] = currLine; // Dynamically add in new elements into array
+            linesSize++;
+            // Create a new dynamic array
+            string* newLines;
+            newLines = new string[linesSize];
+            // Make a copy of the array
+            for (int i = 0; i < linesSize; i++) {
+                newLines[i] = lines[i];
+            }
+            // Reallocate memory but do not want to lose the value of lines
+            // Restore lines to newLines
+            delete [] lines;
+            lines = newLines;
+        }
+    }
+    
+    outLinesArrSize = linesSize;
+    
+    return lines;
+}
+
+vector<string> main2() {
+    
+    vector<string> lines; // Initialize vector
+    bool seenEndOfSequence = false; // Flag for while loop
+    string currLine; // Store user input
+    
+    cout<<"Version B:\n";
+    cout<<"Please enter a sequence of positive integers, each in a separate line.\n"
+        <<"End you input by typing -1.\n";
+    
+    while (seenEndOfSequence == false) {
+        getline(cin, currLine);
+        if (currLine == "-1") // If the user types in -1, break the while loop
+            seenEndOfSequence = true;
+        else
+            lines.push_back(currLine); // Dynamically add in new elements into array
+    }
+    
+    // Return array
+    return lines;
 }
