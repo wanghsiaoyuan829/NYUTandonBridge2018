5eb43bcc095b47e6cf60518ad323f665fd974d25
Nam-H-Nguyen <nguyenbydoing@gmail.com>
2018-06-06 13:52:39 -0700

    JumpIt board sum lowest cost adjacent elements using recursive functions implemented in C++

--- Week 8 - Recursion/homework/nn1338_hw8_q3/nn1338_hw8_q3/nn1338_hw8_q3.cpp
+++ Week 8 - Recursion/homework/nn1338_hw8_q3/nn1338_hw8_q3/nn1338_hw8_q3.cpp
@@ -47,7 +47,47 @@
 #include <iostream>
 using namespace std;
 
+int minInArray1(int arr[], int arrSize);
+int minInArray2(int arr[], int low, int high);
+
 int main() {
     
+    int arr[10] = {9, -2, 14, 12, 3, 6, 2, 1, -9, 15};
+    int res1, res2, res3, res4;
+    
+    res1 = minInArray1(arr, 10);
+    res2 = minInArray2(arr, 0, 9);
+    cout<<res1<<" "<<res2<<endl; //should both be -9
+    
+    res3 = minInArray2(arr, 2, 5);
+    res4 = minInArray1(arr+2, 4); //arr+2 is equivalent to &(arr[2])
+    cout<<res3<<" "<<res4<<endl; //should both be 3
+    
     return 0;
+}
+
+int minInArray1(int arr[], int arrSize) {
+    
+    // Base case: arrSize = 0
+    if (arrSize <= 1)
+        return arr[0];
+    else {
+        int min = minInArray1(arr, arrSize - 1);
+        if (arr[arrSize - 1] < arr[arrSize - 2]) {
+            min = arr[arrSize - 1];
+        }
+        return min;
+    }
+}
+
+int minInArray2(int arr[], int low, int high) {
+    if (low == high)
+        return arr[low];
+    else {
+        int min = minInArray2(arr, low + 1, high);
+        if (arr[low] < min) {
+            min = arr[low];
+        }
+        return min;
+    }
 }
--- Week 8 - Recursion/homework/nn1338_hw8_q4/nn1338_hw8_q4/nn1338_hw8_q4.cpp
+++ Week 8 - Recursion/homework/nn1338_hw8_q4/nn1338_hw8_q4/nn1338_hw8_q4.cpp
@@ -33,7 +33,60 @@
 #include <iostream>
 using namespace std;
 
+int jumpIt(int gameBoardArr[], int gameBoardArrSize, int n);
+
 int main() {
+    int lowCostJump1;
+    int gameBoardArr1[] = {0, 3, 80, 6, 57, 10};
+    int gameBoardArr1Size = 6;
+    
+    int lowCostJump2;
+    int gameBoardArr2[] = {0, 10, 90, 5, 1, 50, 7};
+    int gameBoardArr2Size = 7;
+    
+    int n = 0;
+    
+    for (int i = 0; i < gameBoardArr1Size; i++) {
+        cout<<gameBoardArr1[i]<<" ";
+    }
+    
+    cout<<endl;
+    
+    // Expected output: 19
+    // 3 + 6 + 10
+    lowCostJump1 = jumpIt(gameBoardArr1, gameBoardArr1Size - 1, n);
+    cout<<"Lowest cost by jumping through 1 or 2 adjacent elements in the board is: "<<lowCostJump1<<endl;
+    
+    for (int i = 0; i < gameBoardArr2Size; i++) {
+        cout<<gameBoardArr2[i]<<" ";
+    }
+    
+    cout<<endl;
+    
+    // Expected output: 22
+    // 10 + 5 + 7
+    lowCostJump2 = jumpIt(gameBoardArr2, gameBoardArr2Size - 1, n);
+    cout<<"Lowest cost by jumping through 1 or 2 adjacent elements in the board is: "<<lowCostJump2<<endl;
     
     return 0;
+}
+
+// Input: game board array, array size, and index position to compare elements
+// Output: return the lowest cost value by jumping through adjacent elements in the array
+int jumpIt(int gameBoardArr[], int gameBoardArrSize, int n) {
+    // Base case #1: if current index position is equal to gameBoardArrSize
+    if (n == gameBoardArrSize) {
+        return gameBoardArr[n];
+    // Base case #2: if there are only the first and last element in the array
+    // return the sum of the first element and the last element
+    } else if (n == gameBoardArrSize - 1) {
+        return (gameBoardArr[0] + gameBoardArr[gameBoardArrSize]);
+    // Recursive function: if the previous element is less than the next element
+    // return the sum of the current index to the value of the next element
+    } else if (jumpIt(gameBoardArr, gameBoardArrSize, n + 1) < jumpIt(gameBoardArr, gameBoardArrSize, n + 2))
+        return (gameBoardArr[n] + jumpIt(gameBoardArr, gameBoardArrSize, n + 1));
+    // Recursive function: else the previous element is NOT less than the next element
+    // Jump two indexes and add the value of that index to the current value of the current element
+    else
+        return (gameBoardArr[n] + jumpIt(gameBoardArr, gameBoardArrSize, n + 2));
 }
