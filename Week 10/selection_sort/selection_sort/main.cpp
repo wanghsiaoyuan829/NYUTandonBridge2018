131e2516498d513113dfa5d9c9ce0f1f42dd28e0
Nam-H-Nguyen <nguyenbydoing@gmail.com>
2018-06-20 23:39:27 -0700

    Selection Sort runtime analysis n^2 implemented in C++

--- Week 10/selection_sort/selection_sort/main.cpp
+++ Week 10/selection_sort/selection_sort/main.cpp
@@ -6,10 +6,53 @@
 //  Copyright © 2018 NamHNguyen NYU Tandon School of Engineering Bridge 2018 Program. All rights reserved.
 //
 
+// Selection Sort Algorithm
+// n = high - low + 1
+// T(n) = n + (n-1) + (n-2) + (n-3) + ... + 2 + 1 = theta(n^2)
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void printArray(int arr[], int arrSize) {
+    int i;
+    
+    for (i = 0; i < arrSize; i++)
+        cout<<arr[i]<<' ';
+    cout<<endl;
+    
+}
+
+int findIndexOfMin(int arr[], int low, int high) {
+    int min, minInd;
+    int i;
+    
+    min = arr[low];
+    minInd = low;
+    for (i = low + 1; i <= high; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+            minInd = i;
+        }
+    }
+    
+    return minInd;
+}
+
+void selectionSort(int arr[], int arrSize) {
+    int i, minInd;
+    
+    // Runtime Analysis: T(n) = theta(n - i)
+    for (i = 0; i< arrSize; i++) {
+        minInd = findIndexOfMin(arr, i, arrSize - 1);
+        swap(arr[i], arr[minInd]);
+    }
+}
+
+int main() {
+    int arr[13] = {5, 14, 10, 8, 13, 1, 18, 3, 4, 2, 19, 3, 4};
+    int arrSize = 13;
+    
+    selectionSort(arr, arrSize);
+    printArray(arr, arrSize);
+    
     return 0;
 }
